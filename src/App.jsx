f8563e23a0bf5e09354dec91f29afbbde2db970c
Evgeniy <evg1999prog@gmail.com>
2024-12-24 16:28:50 +0200

    add new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Categories } from "./components/Categories/Categories";
+import { Header } from "./components/Header/Header";
+import { PizzaBlock } from "./components/PizzaBlock/PizzaBlock";
+import { Sort } from "./components/Sort/Sort";
+import "./scss/app.scss";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+    return (
+        <div className="wrapper">
+            <Header />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories />
 
-export default App
+                        <Sort />
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        <PizzaBlock price={500} countProps={43} /> 
+                        <PizzaBlock price={600} countProps={34} />
+                        <PizzaBlock price={700} countProps={56} />
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
