b4f687ead9d6f5d38a8a5840e72500a4afa9b764
Evgeniy <evg1999prog@gmail.com>
2024-12-27 16:18:25 +0200

    get data with API

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { useEffect, useState } from "react";
 import { Categories } from "./components/Categories/Categories";
 import { Header } from "./components/Header/Header";
 import { PizzaBlock } from "./components/PizzaBlock/PizzaBlock";
 import { Sort } from "./components/Sort/Sort";
 import "./scss/app.scss";
 
-import pizzas from "./assets/pizzas.json";
-console.log('pizzas: ', pizzas);
-
 export const App = () => {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("https://66a22dd1967c89168f1f1755.mockapi.io/items")
+            .then((res) => res.json())
+            .then((json) => setItems(json));
+    }, []);
+
     return (
         <div className="wrapper">
             <Header />
@@ -21,7 +27,7 @@ export const App = () => {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {pizzas.map(pizza => (
+                        {items.map((pizza) => (
                             <PizzaBlock key={pizza.id} {...pizza} />
                         ))}
                     </div>
